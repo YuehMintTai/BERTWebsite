62e154529916050d4b534116ca91d6d8eca57dae
YuehMintTai <80385325+YuehMintTai@users.noreply.github.com>
2021-09-04 16:07:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ def index():
   ##myPredict='50%'
   return render_template("index.html",title=title,myHx=myText,myPredict=myPredict)
 
-app.run()
+if __name__=='__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
